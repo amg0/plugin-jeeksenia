02234eeb013e7ba474ca74115661bba4a95048e7
Alexis Mermet <alexis.mermet@gmail.com>
2023-05-02 22:20:45 +0200

    test encrypt

--- core/class/jeeksenia.class.php
+++ core/class/jeeksenia.class.php
@@ -34,8 +34,8 @@ class jeeksenia extends eqLogic {
 	/*
 	* Permet de crypter/décrypter automatiquement des champs de configuration du plugin
 	* Exemple : "param1" & "param2" seront cryptés mais pas "param3"
-	public static $_encryptConfigKey = array('param1', 'param2');
 	*/
+	public static $_encryptConfigKey = array('pincode', 'password');
 
 	/*     * ***********************Methode static*************************** */
 /*     * ***********************Methode static*************************** */
