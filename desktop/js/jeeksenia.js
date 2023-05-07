b03cb6bdeea4ce4455ac9c52f604768525edb4c2
Alexis Mermet <alexis.mermet@gmail.com>
2023-05-07 23:37:45 +0200

    fix

--- desktop/js/jeeksenia.js
+++ desktop/js/jeeksenia.js
@@ -136,7 +136,7 @@ function printEqLogic(eqLogic) {
           return v.trace;
         })
 
-        html = MyArray2Table(arr,'id',['id','date','time','event'],'Events','jeeksenia-cls','jeeksenia-htmlid',false);
+        html = MyArray2Table(arr,'id',['id','data','time','event'],'Events','jeeksenia-cls','jeeksenia-htmlid',false);
         //html = MyArray2Table(arr,idcolumn,viscols,caption,cls,htmlid,bResponsive) 
 				$("#jeeksenia-events").html( html );
       }
